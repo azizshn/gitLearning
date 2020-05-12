4fd6f252e34a0630adee4664354273008056f1e3
azizshn <60260027+azizshn@users.noreply.github.com>
2020-05-12 16:06:39 +0100

    Update Practice.java

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -2,6 +2,9 @@ package tests;
 
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello canim");
+        
+        
+        // this line is written from github
     }
 }
